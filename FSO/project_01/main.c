aa16a60ee3bff7fc7176c83017ab641b2d8c536e
Fernando Aguilar <fernando@aguilar.net.br>
2017-03-18 23:43:53 -0300

    Initializing project 01 files w

---
+++ FSO/project_01/main.c
@@ -0,0 +1,3 @@
+//http://stackoverflow.com/a/15435045
+//http://stackoverflow.com/a/565282/202451
+//http://stackoverflow.com/questions/471962/how-do-determine-if-a-polygon-is-complex-convex-nonconvex
