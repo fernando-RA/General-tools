6b8a5b6b00ddd39c7c13373bea59f31258c517eb
Fernando Aguilar <fernando.aguilar@aeb.gov.br>
2017-03-27 12:37:46 -0300

    Restarting development i

--- FSO/project_01/q01 - Convex Quadrangle/geometric_operations.h
+++
@@ -1,5 +0,0 @@
-double calculate_point_distance(Point p, point q);
-
-double calculate_area(Quadrangle quad);
-
-double calculate_convexity_condition();
--- FSO/project_01/q01 - Convex Quadrangle/main.c
+++ FSO/project_01/q01 - Convex Quadrangle/main.c
@@ -6,48 +6,10 @@
 #include <stdio.h>
 #include <math.h>
 
-#include "composed_types.h"
+#include "composedTypes.h"
 
-double calculate_point_distance(Point p, Point q)
-{
-  return sqrt(pow((q.x)-(p.x), 2) + pow((q.y) - (p.y) , 2));
-}
-
-Quadrangle read_input_points(){
-  Point a, b, c, d;
-  Quadrangle new;
-
-  printf("a(x,y)\n");
-  scanf("%lf %lf", &a.x, &a.y);
-  printf("b(x,y)\n");
-  scanf("%lf %lf", &b.x, &b.y);
-  printf("c(x,y)\n");
-  scanf("%lf %lf", &c.x, &c.y);
-  printf("d(x,y)\n");
-  scanf("%lf %lf", &d.x, &d.y);
-
-  new.dist_ab = calculate_point_distance(a, b);
-
-  new.dist_bc = calculate_point_distance(b, c);
-
-  printf("TSTE: %lf\n", new.dist_bc);
-
-  new.dist_cd = calculate_point_distance(c, d);
-
-  new.dist_da = calculate_point_distance(d, a);
-
-  return new;
-}
 
 int main(){
-  //Quadrangle* new = (Quadrangle*) malloc(siezof(Quadrangle));
-  Quadrangle new;
-  new = read_input_points();
-
-  printf("DIST_ab: %.2lf\n", new.dist_ab);
-  printf("DIST_bc: %.2lf\n", new.dist_bc);
-  printf("DIST_cd: %.2lf\n", new.dist_cd);
-  printf("DIST_da: %.2lf\n", new.dist_da);
-
+  printf("Hello World\n");
   return 0;
 }
