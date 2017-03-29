f7585cbabf61c3a7e66a85f647c73759e3c5f67e
Fernando Aguilar <fernando@aguilar.net.br>
2017-03-29 19:46:51 -0300

    Evolving q02, adding general docs

--- FSO/project_01/q02 - Number of Arguments/main.c
+++ FSO/project_01/q02 - Number of Arguments/main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
 int main(int argc, char** argv) {
-    printf("Have %d arguments:\n", argc-1);
-    for (int i = 1; i < argc; ++i) {
-        printf("%s\n", argv[i]);
-    }
+  int i;
+
+  printf("# de parametros: %d\n", argc-1);
+  printf("Nome do executavel: %s\n", argv[0]);
+
+  for (i = 1; i < argc; ++i) {
+    printf("Parametro #%d: %s\n", i, argv[i]);
+  }
+  return 0;
 }
