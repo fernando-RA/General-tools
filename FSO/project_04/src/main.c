e02d8a0f827a102214219fc93473451aa17b5dc8
Fernando Aguilar <fernando@aguilar.net.br>
2017-05-30 12:11:20 -0300

    calculating sine from agrv parameter

--- FSO/project_04/src/main.c
+++ FSO/project_04/src/main.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
 #include "libseno.h"
+#include <stdlib.h>
+#include <string.h>
 
-int main(){
+int main(int argc, char** argv) {
+    int i;
+    double angle;
+    if(argc-1 == 0){
+        printf("No parameters passed\n");
+        return 0;
+    }
 
-  printf("resposta sine = %lf\n", seno(1.57));
+    for (i = 1; i < argc; ++i) {
+        printf("Parametro #%d: %s\n", i, argv[i]);
 
-  return 0;
+        if(strcmp(argv[i], "-s") == 0){
+            angle = strtod(argv[i+1], NULL);
+
+            printf("seno (%.3lf) = %lf\n", angle, seno(angle));
+        }
+    }
+
+    return 0;
 }
