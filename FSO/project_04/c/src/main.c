655b3a84eb63cacbfdc6639bc82e5dd17ebe4306
Fernando Aguilar <fernando@aguilar.net.br>
2017-06-01 02:37:35 -0300

    Adding dynamic load execution

--- FSO/project_04/c/src/main.c
+++ FSO/project_04/c/src/main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <dlfcn.h>
 
 #include "libseno.h"
+
+void dynamic_load(double, char*);
 
 int main(int argc, char** argv) {
   int i;
@@ -15,19 +17,37 @@ int main(int argc, char** argv) {
 
   else{
     angle = strtod(argv[2], NULL);
-    m_sine = seno(angle);
-    m_arco_seno = arco_seno(m_sine);
-
+    
     for (i = 1; i < argc; ++i) {
       if(strcmp(argv[i], "-s") == 0){
+        dynamic_load(angle, "seno");
         printf("seno (%.2lf) = %.4lf\n", angle, m_sine);
       }
 
+      dynamic_load(angle, "arco_seno");
       else if (strcmp(argv[i], "-a") == 0){
         printf("arc_seno (%.2lf) = %.4lf\n", m_sine, m_arco_seno);
       }
     }
   }
-
   return 0;
+}
+
+void dynamic_load(double rads, char * function_name){
+  void * handle = dlopen("libseno.so", RTLD_LAZY);
+  if(!handle) {
+    printf("%s\n", dlerror());
+    exit(EXIT_FAILURE);
+  }
+
+  dlerror();
+  double (*function)(double);
+  *(void **) (&function) = dlsym(handle, function_name);
+  char *error = dlerror();
+
+  if(error != NULL){
+    printf("%s\n", error);
+    exit(1);
+  }
+  dlclose(handle);
 }
