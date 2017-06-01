063cc37575eded5ba21e0807ab2a3bce7efdf20d
Fernando Aguilar <fernando@aguilar.net.br>
2017-06-01 00:37:55 -0300

    Adding control to arc_seno on main

--- FSO/project_04/src/main.c
+++ FSO/project_04/src/main.c
@@ -4,22 +4,33 @@
 #include <string.h>
 
 int main(int argc, char** argv) {
-    int i;
-    double angle;
-    if(argc-1 == 0){
-        printf("No parameters passed\n");
-        return 0;
-    }
+  int i;
+  double angle = 0, sine = 0;
 
-    for (i = 1; i < argc; ++i) {
-        printf("Parametro #%d: %s\n", i, argv[i]);
-
-        if(strcmp(argv[i], "-s") == 0){
-            angle = strtod(argv[i+1], NULL);
-
-            printf("seno (%.3lf) = %lf\n", angle, seno(angle));
-        }
-    }
-
+  if(argc-1 == 0){
+    printf("No parameters passed\n");
     return 0;
+  }
+
+  for (i = 1; i < argc; ++i) {
+    printf("Parametro #%d: %s\n", i, argv[i]);
+
+    if(strcmp(argv[i], "-s") == 0){
+      angle = strtod(argv[i+1], NULL);
+
+      printf("seno (%.3lf) = %lf\n", angle, seno(angle));
+    }
+
+    else if (strcmp(argv[i], "-a") == 0){
+      sine = strtod(argv[i+1], NULL);
+
+      printf("arc_seno (%.3lf) = %lf\n", sine, arc_seno(sine) );
+    }
+    else{
+      printf("Invalid parameters passing. Exiting");
+      exit(1);
+    }
+  }
+
+  return 0;
 }
