57ee3dfeaa1bb7b1fbd6f91cbcd9d98b84a8e8a8
Fernando Aguilar <fernando@aguilar.net.br>
2017-09-06 11:03:34 -0300

    refactoring my_hanoi and validating inputs

--- algorithms/hanoi_tower/teste_python/my_hanoi.py
+++ algorithms/hanoi_tower/teste_python/my_hanoi.py
@@ -5,57 +5,71 @@ from string import ascii_uppercase
 pegs = ascii_uppercase[:NUM_PEGS]
 
 def main():
-
     # Build graph
     G = build()
 
+    #Validation
+    checkDisksOrder("source.txt")
+    checkDisksOrder("destination.txt")
+
     # Sample
-    initial_state = readInitialState("input.txt")
-    #checkDisksOrder(initial_state)
+    initial_state = readInitialState("source.txt")
     final_state = readFinalState()
-    print initial_state
-    print final_state
+
     # Solution
     path = bfs(G, get_state_id(initial_state), get_state_id(final_state))[::-1]
+    print "\nSolution:"
     for i in range(len(path)-1):
         print(move(get_state(path[i]), get_state(path[i+1])))
 
+def openFile(filename):
+    lines = open(filename, 'r')
+    file.close
+    return lines
+
+def checkDisksOrder(filename):
+    state_map = {}
+    # Save state in map
+    for line in openFile(filename):
+        peg_pair = line.strip().split('-')
+        peg_pair = line.strip().split('-')
+        state_map[peg_pair[0]] = peg_pair[1].split(':')
+
+    # Check for wrong cases in map
+    for peg in state_map:
+        array_size = len(state_map[peg])
+        if(array_size) > 1:
+            for disk, valor in enumerate(state_map[peg]):
+                if(disk < array_size - 1):
+                    next_disk = state_map[peg][(disk + 1)]
+                if(next_disk > valor):
+                    print("ERROR: Disk '" + str(valor) + "' is on top of disk: '" + str(next_disk) + "'\n")
+                    raise ValueError, "Invalid State"
+    return True
+
 def readInitialState(filename):
     state_map = [None] * NUM_DISCS
-    lines = open(filename, 'r')
-    for line in lines:
+
+    for line in openFile(filename):
         peg_pair = line.strip().split('-')
+        # if there is a disk on peg
         if peg_pair[1] is not '':
             disks_in_peg = enumerate(peg_pair[1].split(':'))
             for disk, value in disks_in_peg :
                 state_map[int(value)-1] = peg_pair[0]
-    file.close
     return state_map
 
 def readFinalState():
     final_state_map = [None] * NUM_DISCS
     for peg in range(NUM_PEGS):
         final_state_configuration = raw_input("Ordem final de " + str(pegs[peg]) + ':\n')
-        print (final_state_configuration)
-
         peg_pair = final_state_configuration.strip().split('-')
+        # if there is a disk on peg
         if peg_pair[1] is not '':
             disks_in_peg = enumerate(peg_pair[1].split(':'))
             for disk, value in disks_in_peg :
                 final_state_map[int(value)-1] = peg_pair[0]
     return final_state_map
-
-def checkDisksOrder(mapa):
-    for peg in mapa:
-        array_size = len(mapa[peg])
-        if(array_size) > 1:
-            for disk, valor in enumerate(mapa[peg]):
-                if(disk < array_size - 1):
-                    next_disk = mapa[peg][(disk + 1)]
-                if(next_disk > valor):
-                    print("O disco " + str(valor) + " esta em cima do disco " + str(next_disk) + "\n")
-                    return False
-    return True
 
 def get_state(state_id):
     state = []
@@ -66,34 +80,34 @@ def get_state(state_id):
 
 def get_state_id(state):
     state_id = 0
-    for disc in range(NUM_DISCS):
-        state_id += NUM_PEGS**disc * pegs.index(state[disc])
+    for disk in range(NUM_DISCS):
+        state_id += NUM_PEGS**disk * pegs.index(state[disk])
     return state_id
 
-def get_peg(state_id, disc):
+def get_peg(state_id, disk):
     state = get_state(state_id)
-    return pegs.index(state[disc])
+    return pegs.index(state[disk])
 
-def fits_on_top(state_id, disc, peg):
+def fits_on_top(state_id, disk, peg):
     state = get_state(state_id)
-    for i in range(disc):
+    for i in range(disk):
         if state[i] == pegs[peg]:
             return False
     return True
 
 def get_valid_moves(state_id):
     valid_moves = []
-    for disc in range(NUM_DISCS):
-        disc_peg = get_peg(state_id,disc)
-        if not fits_on_top(state_id, disc, disc_peg):
+    for disk in range(NUM_DISCS):
+        disk_peg = get_peg(state_id,disk)
+        if not fits_on_top(state_id, disk, disk_peg):
             continue
         for peg in range(NUM_PEGS):
-            if peg == disc_peg:
+            if peg == disk_peg:
                 continue
-            if not fits_on_top(state_id, disc, peg):
+            if not fits_on_top(state_id, disk, peg):
                 continue
             state = get_state(state_id)
-            state[disc] = pegs[peg]
+            state[disk] = pegs[peg]
             valid_moves += [get_state_id(state)]
     return valid_moves
 
@@ -130,9 +144,9 @@ def bfs(G, src, dst):
     return path
 
 def move(src, dst):
-    for i in range(NUM_DISCS):
-        if src[i] != dst[i]:
-            return '%d-%c' % (i+1, dst[i])
+    for disk in range(NUM_DISCS):
+        if src[disk] != dst[disk]:
+            return '%d-%c' % (disk+1, dst[disk])
 
 if __name__ == "__main__":
    main()
